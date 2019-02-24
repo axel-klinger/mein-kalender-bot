567efbecf593f70fd525c4ffd63afa2e8e22670f
Axel Klinger <axel.klinger@web.de>
2019-02-24 14:28:48 +0100

    Fix kalender-training.py

--- kalender-training.py
+++ kalender-training.py
@@ -3,16 +3,16 @@ from rasa_nlu.model import Trainer
 from rasa_nlu import config
 from rasa_nlu.model import Metadata, Interpreter
 
-def train_kalender(data, configs, model_dir):
+def train_kalender_intent(data, configs, model_dir):
 	training_data = load_data(data)
 	trainer = Trainer(config.load(configs))
 	trainer.train(training_data)
 	model_directory = trainer.persist(model_dir, fixed_model_name = 'kalenderbot')
 
-def run_nlu():
+def run_kalender_intent():
 	interpreter = Interpreter.load('./models/nlu/default/kalenderbot')
 	print(interpreter.parse(u"Kannst du mir sagen wann die Weihnachtsferien sind."))
 
 if __name__ == '__main__':
-	train_nlu('./data/data.json', 'config.json', './models/nlu')
-	run_nlu()
+	train_kalender_intent('./data/data.json', 'config.json', './models/nlu')
+	run_kalender_intent()
