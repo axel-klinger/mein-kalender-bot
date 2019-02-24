007ed0edb62e284a2371cf1a3913a83f5c401e43
Axel Klinger <axel.klinger@web.de>
2019-02-24 14:26:12 +0100

    Integrate intent training test into training

--- kalender-training-test.py
+++
@@ -1,7 +0,0 @@
-from rasa_nlu.model import Interpreter
-
-def predict_intent (text):
-    interpreter = Interpreter.load('./models/nlu/default/kalenderbot')
-    print (interpreter.parse(text))
-
-predict_intent("Kannst du mir sagen wann die Weihnachtsferien sind")
--- kalender-training.py
+++ kalender-training.py
@@ -1,11 +1,18 @@
 from rasa_nlu.training_data import load_data
 from rasa_nlu.model import Trainer
 from rasa_nlu import config
+from rasa_nlu.model import Metadata, Interpreter
 
-def train_calendar (data_json, config_file, model_dir):
-    training_data = load_data(data_json)
-    trainer = Trainer(config.load(config_file))
-    trainer.train(training_data)
-    model_directory = trainer.persist(model_dir, fixed_model_name = 'kalenderbot')
+def train_kalender(data, configs, model_dir):
+	training_data = load_data(data)
+	trainer = Trainer(config.load(configs))
+	trainer.train(training_data)
+	model_directory = trainer.persist(model_dir, fixed_model_name = 'kalenderbot')
 
-train_calendar('./data/data.json', 'config.json', './models/nlu')
+def run_nlu():
+	interpreter = Interpreter.load('./models/nlu/default/kalenderbot')
+	print(interpreter.parse(u"Kannst du mir sagen wann die Weihnachtsferien sind."))
+
+if __name__ == '__main__':
+	train_nlu('./data/data.json', 'config.json', './models/nlu')
+	run_nlu()
